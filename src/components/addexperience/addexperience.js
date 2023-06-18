a9c9f4faca91de96bdbd00ee6faaa34c8068a08a
Vasu Kaushik <vasu.kaushik@tcs.com>
2023-06-18 14:38:41 +0530

    enhancements

--- src/components/addexperience/addexperience.js
+++ src/components/addexperience/addexperience.js
@@ -6,6 +6,7 @@ import Grid from '@mui/material/Unstable_Grid2';
 import "./addexperience.css"
 import Button from '@mui/material/Button';
 import {useState} from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 import { useNavigate } from 'react-router-dom';
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -18,67 +19,115 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function AddExperience() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const [onTitleEmpty,setTitleEmpty]=useState(false);
+  const [onDescriptionEmpty,setDescEmpty]=useState(false);
+  const [loader,setLoader]=useState(false);
   const navigate=useNavigate();
   function handleTitle(event){
     setTitle(event.target.value);
+    setTitleEmpty(false);
     console.log(event.target.value);
   }
 
   function handleDescription(event){
     setDescription(event.target.value);
+    setDescEmpty(false);
     console.log(event.target.value);
   }
 
   function handleSubmit(){
-    console.log("title------>",title,"description------------->",description);
-    const addLog = async () => {
-      try {
-          const requestOptions = {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({ title:title, description:description })
-          };
-         await fetch('https://travel-log-backend.onrender.com/logs', requestOptions)
-              .then(response => response.json())
-              .then(data => {
-                console.log("data---->",data);
-                navigate('/')
-      });
-      } catch (error) {
-          console.log(error);
-      }
-  };
-  addLog();
     
+    if(title==='' && description===''){
+      setDescEmpty(true);
+      setTitleEmpty(true);
+    }
+    else if(title===''){
+ 
+      setTitleEmpty(true);
+    }
+    else if(description===''){
+      setDescEmpty(true);
+
+    }
+    else{
+      setLoader(true);
+      console.log("title------>",title,"description------------->",description);
+      const addLog = async () => {
+        try {
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ title:title, description:description })
+            };
+           await fetch('https://travel-log-backend.onrender.com/logs', requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                  setLoader(false);
+                  console.log("data---->",data);
+                  
+                  navigate('/',{
+                    state: {
+                      added:true
+                    }
+                  })
+        });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    addLog();
+    }
+    
+  
+    
+  }
+
+  function handleBlurTitle(){
+      if(title.trim()===''){
+        console.log("title is empty");
+        setTitleEmpty(true);
+      }
+  }
+  function handleBlurDescription(){
+    if(description.trim()===''){
+      console.log("description is empty");
+      setDescEmpty(true);
+    }
   }
   return (
     <>
     <Box sx={{ flexGrow: 1 , padding:"10%" }}>
       <Grid container spacing={2}>
         <Grid xs={4}>
-          <Item>Title</Item>
+          <Item>Title*</Item>
         </Grid>
         <Grid xs={8}>
           <Item>
           
-            <textarea value={title} onChange={handleTitle} className='text'></textarea>
+            <textarea value={title} onChange={handleTitle} onBlur={handleBlurTitle} className='text'></textarea>
+           
           </Item>
+          {onTitleEmpty&&<p className='error'>*Title is required</p>}
         </Grid>
 
         <Grid xs={4}>
-          <Item>Experience</Item>
+          <Item>Experience*</Item>
         </Grid>
         <Grid xs={8}>
-          <Item style={{height: "347%"}}>
+          <Item >
           
-            <textarea value={description} onChange={handleDescription} className='text'></textarea>
+            <textarea style={{height: "145px"}}value={description} onChange={handleDescription} onBlur={handleBlurDescription} className='text'></textarea>
+            
           </Item>
+          {onDescriptionEmpty&&<p className='error'>*Description is required</p>}
         </Grid>
+       
       </Grid>
       
     </Box>
     <div className='shareButton'>
-    <Button onClick={handleSubmit} variant="contained">Add Your Experience</Button>
+    {!loader&&<Button onClick={handleSubmit} variant="contained">Add Your Experience</Button>}
+    {loader&&<CircularProgress />}
     </div>
     </>
     
--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -4,13 +4,33 @@ import './dashboard.css'
 import Experience from '../experience/experience'
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import MuiAlert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export default function Dashoard() {
   const navigate = useNavigate();
   const [logs,setLogs]=React.useState([]);
+  const [open, setOpen] = React.useState(false);
+  const [flag,setFlag]=React.useState(0);
+  const { state } = useLocation();
+
   function navigateToAddExperience() {
     navigate('/addExperience');
   }
+  console.log("state------>",state);
+  if(state && state.added && !open && flag==0){
+    setOpen(true);
+    setTimeout(() => {
+      setFlag(1);
+      setOpen(false)
+    }, 2000);
+  }
+  
 
   const fetchLogs = async () => {
     try {
@@ -38,12 +58,17 @@ export default function Dashoard() {
     <div className='container'>
       <Button onClick={navigateToAddExperience} variant="contained">Share Your Experience</Button>
       <p>Already Shared Experiences</p>
+      <Snackbar open={open} autoHideDuration={2000} >
+        <Alert severity="success" sx={{ width: '100%' }}>
+          Experience added successfully!
+        </Alert>
+      </Snackbar>
       {
         logs.map((log,index)=>{
           return <Experience key={index} data={log} />
         })
       }
- 
+      
     </div>
   );
 }
